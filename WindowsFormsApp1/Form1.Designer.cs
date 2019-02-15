cadc9519bc71a157243831e277e12399671c9888
Катышев Павел Александрович <katyshev.p@ccam.cmc.unn.net>
2019-02-15 12:19:27 +0300

    new

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace WindowsFormsApp1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.фильтрыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.точечныеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.матричныеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.инверсияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.фильтрыToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(800, 423);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 1;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+			// 
+			// файлToolStripMenuItem
+			// 
+			this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.открытьToolStripMenuItem});
+			this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+			this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+			this.файлToolStripMenuItem.Text = "Файл";
+			// 
+			// фильтрыToolStripMenuItem
+			// 
+			this.фильтрыToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.точечныеToolStripMenuItem,
+            this.матричныеToolStripMenuItem});
+			this.фильтрыToolStripMenuItem.Name = "фильтрыToolStripMenuItem";
+			this.фильтрыToolStripMenuItem.Size = new System.Drawing.Size(69, 20);
+			this.фильтрыToolStripMenuItem.Text = "Фильтры";
+			// 
+			// точечныеToolStripMenuItem
+			// 
+			this.точечныеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.инверсияToolStripMenuItem});
+			this.точечныеToolStripMenuItem.Name = "точечныеToolStripMenuItem";
+			this.точечныеToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.точечныеToolStripMenuItem.Text = "Точечные";
+			// 
+			// матричныеToolStripMenuItem
+			// 
+			this.матричныеToolStripMenuItem.Name = "матричныеToolStripMenuItem";
+			this.матричныеToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.матричныеToolStripMenuItem.Text = "Матричные";
+			// 
+			// инверсияToolStripMenuItem
+			// 
+			this.инверсияToolStripMenuItem.Name = "инверсияToolStripMenuItem";
+			this.инверсияToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.инверсияToolStripMenuItem.Text = "Инверсия";
+			// 
+			// открытьToolStripMenuItem
+			// 
+			this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+			this.открытьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.открытьToolStripMenuItem.Text = "Открыть";
+			this.открытьToolStripMenuItem.Click += new System.EventHandler(this.открытьToolStripMenuItem_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem фильтрыToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem точечныеToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem инверсияToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem матричныеToolStripMenuItem;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+	}
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+	public partial class Form1 : Form
+	{
+		Bitmap image;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.Filter = "Image files | *.png; *.jpg; *.bmp | All Files (*.*) | *.*";
+			if (dialog.ShowDialog() == DialogResult.OK)
+			{
+			}
+			image = new Bitmap(dialog.FileName);
+			pictureBox1.Image = image;
+			pictureBox1.Refresh();
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
